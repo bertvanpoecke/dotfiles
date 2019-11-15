6ad1a47e782fd1942417706bc663378df705b447
Bert Van Poecke <bertvanpoecke@hotmail.com>
2019-11-15 10:25:51 +0100

    Update vs code settings and add intallation script to install file

--- vscode/install_vscode_settings.sh
+++ vscode/install_vscode_settings.sh
@@ -4,7 +4,7 @@ read -p "Do you want to install the Visual Studio Code settings? Remark this wil
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit; fi
 
 filename="settings.json"
-if is-macos; then
+if ../bin/is-macos; then
     folder="$HOME/Library/Application Support/Code/User/"
 else
     folder="$HOME/.config/Code/User/"
@@ -20,9 +20,11 @@ filepath="${folder}${filename}"
 echo $filepath
 
 if [[ -e $filepath ]]; then
+    echo "VSCode settings were already present. Renaming old settings to settings_old.json..."
     cp "$filepath" "${filepath}_old"
+    rm -f "$filepath" 2>/dev/null
 fi
 
-cp "$(pwd)/$filename" "$filepath"
+ln -s "$(pwd)/$filename" "$filepath"
 
 echo "Done."
