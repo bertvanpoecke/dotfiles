a551110c76d368db5357e1be49f1ee453c6d5ac8
Bert Van Poecke <6654029+bertvanpoecke@users.noreply.github.com>
2020-01-06 15:23:52 +0100

    Add office function to open office applications and add local aliases, if present, in fisshelp

--- fish/functions/fishhelp.fish
+++ fish/functions/fishhelp.fish
@@ -7,4 +7,10 @@ function fishhelp --description 'Show all custom defined aliases and functions.'
 	for file in $FISHDOTFILES/functions/*.fish
 		cat $file | grep '^function'
 	end
+	if test -e "$FISHDOTFILES/local.fish"
+		echo
+		echo
+		echo "## LOCAL ALIASES:"
+		cat "$FISHDOTFILES/local.fish" | grep '^alias'
+	end
 end
